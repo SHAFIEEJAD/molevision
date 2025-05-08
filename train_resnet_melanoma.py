f8821f925f5b9117bf1818d710fe8e17364a4860
SHAFIEEJAD <mhdjaddini@gmail.com>
2025-05-08 08:51:16 +0200

    fix bug  mobilenet

--- train_resnet_melanoma.py
+++ train_resnet_melanoma.py
@@ -72,9 +72,9 @@ train_transform = transforms.Compose([
     transforms.RandomRotation(20),
     transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
     transforms.RandomPerspective(distortion_scale=0.2, p=0.5),
-    transforms.RandomErasing(p=0.5),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
     transforms.ToTensor(),
+    transforms.RandomErasing(p=0.5),
     transforms.Normalize([0.5]*3, [0.5]*3)
 ])
 
