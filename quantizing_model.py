c5523a3250b1037747361d3409e90df6d13cbd2e
SHAFIEEJAD <mhdjaddini@gmail.com>
2025-05-19 12:43:59 +0200

    add latest model

--- quantizing_model.py
+++ quantizing_model.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 
 model = mobilenet_v3_large()
 model.classifier[3] = nn.Linear(model.classifier[3].in_features, 2)
-model.load_state_dict(torch.load("latest_model_epoch20.pth"))
+model.load_state_dict(torch.load("latest_model_epoch20.pth", map_location=torch.device("cpu")))
+#model.load_state_dict(torch.load("latest_model_epoch20.pth"))
 model.eval()
 
 dummy_input = torch.randn(1, 3, 512, 512)
